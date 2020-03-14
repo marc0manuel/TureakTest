fc3cd0f2f32ab7ebb731c81a1d16172d9d3c9e4b
Marco Manuel <mmanuel5@uwo.ca>
2020-03-13 21:11:33 -0400

    Added Amadeus API info

---
+++ js/getData.js
@@ -0,0 +1,29 @@
+function getData(sAirport, fAirport, sDate, fDate, seatClass) {
+
+console.log("You are leaving from: " + sAirport + " heading to: " + fAirport + ". Leaving on " + sDate + " and returning on " + fDate)
+console.log("You have requested seat(s) in: " + seatClass)
+
+var Amadeus = require('amadeus');
+
+var amadeus = new Amadeus({
+  clientId: 'qFWSyWZObr4TMneA2obkDuAMsgtYkIVL',
+  clientSecret: '0mcuJNFhsfEU0Tt8'
+});
+
+amadeus.shopping.flightOffersSearch.get({
+    originLocationCode: 'YYZ',
+    destinationLocationCode: 'LGW',
+    departureDate: '2020-04-01',
+    returnDate: '2020-04-16',
+    adults: '2'
+}).then(function(response){
+  console.log(response.data);
+}).catch(function(responseError){
+  console.log(responseError.code);
+});
+
+
+
+
+
+}
